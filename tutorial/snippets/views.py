7f0c8395431a07f56e4a5993c8e5cfb58fa5eeaa
Kristina <kristina.tibeykina04@mail.ru>
2023-11-08 12:12:57 +0700

    переопределим метод .perform_create()

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -12,6 +12,10 @@ class SnippetList(generics.ListCreateAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
 
+   def perform_create(self, serializer):
+       serializer.save(owner=self.request.user)
+
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
    queryset = Snippet.objects.all()
@@ -26,3 +30,4 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
    queryset = User.objects.all()
    serializer_class = UserSerializer
+
